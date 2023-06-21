6a010be0a54644f55cea5144b4e00af3c29b6b72
sydvys <aysuseyidova36@gmail.com>
2023-06-21 10:59:56 +0400

    centred quiz page and changed scroll bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import { useState } from 'react';
+import { Scrollbars } from 'react-custom-scrollbars';
 import './App.css';
 import Router from './Components/Router/Router';
+
 
 function App() {
 
   return (
     <>
-      <Router />
+      <Scrollbars style={{ width: "100vw", height: "100vh" }}>
+        <Router />
+      </Scrollbars>
     </>
   );
 }
--- src/Components/Pages/Quiz/Quiz.jsx
+++ src/Components/Pages/Quiz/Quiz.jsx
@@ -6,7 +6,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { QUIZ_CONTEXT } from "../../Contexts/Contexts";
 import Styles from './Quiz.module.css';
 import { useNavigate } from 'react-router-dom';
-import { Button } from '@mui/material';
+import { Box, Button } from '@mui/material';
 
 
 
@@ -20,17 +20,17 @@ const Quiz = ({ }) => {
     const updatedQuestions = [...questions];
     updatedQuestions[index].radio = selectedAnswer;
     setQuestions(updatedQuestions);
-  
+
     const currentQuestion = updatedQuestions[index];
     const isCorrect = currentQuestion.correct_answer === selectedAnswer;
-  
+
     let updatedScore = 0;
     for (const question of updatedQuestions) {
       if (question.correct_answer === question.radio) {
         updatedScore++;
       }
     }
-  
+
     setScore(updatedScore);
 
     const updatedSelectedFlags = [...selectedFlags];
@@ -40,49 +40,53 @@ const Quiz = ({ }) => {
 
   return (
     <>
-      {
-        questions.map((question, index) => {
-          console.log(question.question);
-          return (
-            <div key={uuidv4()} >
-              <h3>Question {index + 1}</h3>
-              <p>{he.decode(question.question)}</p>
+      <Box
+        className={Styles.quiz}
+      >
+        {
+          questions.map((question, index) => {
+            console.log(question.question);
+            return (
+              <div
+                key={uuidv4()} >
+                <h3>Question {index + 1}</h3>
+                <p>{he.decode(question.question)}</p>
 
-              <Radio
-                value="True"
-                onChange={(event) => handleRadioChange(event, index)}
-                checked={question.radio === "True"}
-              />
-              <label>t</label>
+                <Radio
+                  value="True"
+                  onChange={(event) => handleRadioChange(event, index)}
+                  checked={question.radio === "True"}
+                />
+                <label>t</label>
 
-              <Radio
-                value="False"
-                onChange={(event) => handleRadioChange(event, index)}
-                checked={question.radio === "False"}
-              />
-              <label>f</label>
+                <Radio
+                  value="False"
+                  onChange={(event) => handleRadioChange(event, index)}
+                  checked={question.radio === "False"}
+                />
+                <label>f</label>
 
-              {selectedFlags[index] && (
-              question.correct_answer === question.radio ? (
-                <Check className={Styles.true} />
-              ) : (
-                <Clear className={Styles.false} />
-              )
-            )}
-            </div>
+                {selectedFlags[index] && (
+                  question.correct_answer === question.radio ? (
+                    <Check className={Styles.true} />
+                  ) : (
+                    <Clear className={Styles.false} />
+                  )
+                )}
+              </div>
+            )
+          }
           )
         }
-        )
-      }
 
-      <Button
-        sx={{ my: 3, px: 5 }}
-        size="large"
-        variant="contained"
-        onClick={() => navigate("/Result")}>
-        get score
-      </Button>
-
+        <Button
+          sx={{ my: 3, px: 5 }}
+          size="large"
+          variant="contained"
+          onClick={() => navigate("/Result")}>
+          get score
+        </Button>
+      </Box>
     </>
   )
 }
